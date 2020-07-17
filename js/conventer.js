866fc2a0c5a0ff5fbe532a682af4e0e2d487ea3b
Olga Sowinska <olga.sowinska00@gmail.com>
2020-07-17 13:53:55 +0200

    added function in js

--- js/conventer.js
+++ js/conventer.js
@@ -1,74 +1,73 @@
-let formElement = document.querySelector(".conventer");
-let currentCurrency = document.querySelector(".js-initial");
-let conventerResult = document.querySelector(".conventer__result");
-let fromCurrency = document.querySelector(".js-from");
-let toCurrency = document.querySelector(".js-to");
+{
+    init();
 
+    const fromCurrency = document.querySelector(".js-from");
+    const toCurrency = document.querySelector(".js-to");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    function myResult(amount, currency, secondCurrency) {
 
-    let amount = +currentCurrency.value;
-    let currency = fromCurrency.value;
-    let secondCurrency = toCurrency.value;
-
-    if (fromCurrency.value === toCurrency.value) {
-        conventerResult.innerText = amount;
-        console.log(amount);
-    } else {
-        switch (currency) {
-            case "EUR":
-                switch (secondCurrency) {
-                    case "PLN":
-                        result = amount * 4.47;
-                        break;
-                    case "USD":
-                        result = amount * 1.13;
-                        break;
-                    case "CZK":
-                        result = amount * 26.68;
-                        break;
-                }
-                break;
-            case "PLN":
-                switch (secondCurrency) {
-                    case "EUR":
-                        result = amount * 0.22;
-                        break;
-                    case "USD":
-                        result = amount * 0.25;
-                        break;
-                    case "CZK":
-                        result = amount * 5.96;
-                        break;
-                }
-                break;
-            case "USD":
-                switch (secondCurrency) {
-                    case "PLN":
-                        result = amount * 3.96;                           
-                        break;
-                    case "EUR":
-                        result = amount * 0.89;
-                        break;
-                    case "CZK":
-                        result = amount * 23.62;
-                        break;
-                }
-                break;
-            case "CZK":
-                switch (secondCurrency) {
-                    case "PLN":
-                        result = amount * 0.17;                           
-                        break;
-                    case "USD":
-                        result = amount * 0.042;
-                        break;
-                    case "EUR":
-                        result = amount * 0.037;
-                        break;
-                }
-                break;
+        if (fromCurrency.value === toCurrency.value) {
+            conventerResult.innerText = amount;
+        } else {
+            switch (currency) {
+                case "EUR":
+                    switch (secondCurrency) {
+                        case "PLN":
+                            return amount * 4.47;
+                        case "USD":
+                            return amount * 1.13;
+                        case "CZK":
+                            return amount * 26.68;
+                    }
+                    break;
+                case "PLN":
+                    switch (secondCurrency) {
+                        case "EUR":
+                            return amount * 0.22;
+                        case "USD":
+                            return amount * 0.25;
+                        case "CZK":
+                            return amount * 5.96;
+                    }
+                    break;
+                case "USD":
+                    switch (secondCurrency) {
+                        case "PLN":
+                            return amount * 3.96;
+                        case "EUR":
+                            return amount * 0.89;
+                        case "CZK":
+                            return amount * 23.62;
+                    }
+                    break;
+                case "CZK":
+                    switch (secondCurrency) {
+                        case "PLN":
+                            return amount * 0.17;
+                        case "USD":
+                            return amount * 0.042;
+                        case "EUR":
+                            return amount * 0.037;
+                    }
+                    break;
+            }
         }
-        conventerResult.innerText = result.toFixed(2);
-    }});
+    }
+
+    function onFormSubmit(event) {
+        event.preventDefault();
+
+        const currentCurrency = document.querySelector(".js-initial");
+        const conventerResult = document.querySelector(".conventer__result");
+
+        const result = myResult(+currentCurrency.value, fromCurrency.value, toCurrency.value);
+        conventerResult.innerText = +result.toFixed(2);
+    }
+
+    function init() {
+        const formElement = document.querySelector(".conventer");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    
+    }
+}
